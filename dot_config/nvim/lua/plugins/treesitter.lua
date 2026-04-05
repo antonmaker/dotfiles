9bd8283efc498d10c16f2c3d8636cec8fde80a0e
Anton <antonkham@gmail.com>
2026-04-05 23:04:15 +0900

    [neovim] fix treesitter to master branch

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,20 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "tsx", "graphql", "css", "scss", "prisma", "json" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "c", "lua", "vim", "vimdoc", "query",
+        "elixir", "heex",
+        "javascript", "typescript", "tsx",
+        "html", "css", "scss",
+        "graphql", "json", "prisma",
+      },
       sync_install = false,
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
